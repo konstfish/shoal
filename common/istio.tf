bac5636dd3fc762511be574830960aaf63014571
David Fischer <david@konst.fish>
2024-03-30 23:46:16 +0100

    feat(istio): some progress on multicluster

--- common/istio.tf
+++ common/istio.tf
@@ -1,14 +1,22 @@
-resource "helm_release" "istio_tetra" {
-  provider         = helm.tetra
-  name             = "istio-base"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "base"
-  namespace        = "istio-system"
-  create_namespace = true
+resource "kubernetes_namespace" "istio_guppy" {
+  provider         = kubernetes.guppy
 
-  set {
-    name = "defaultRevision"
-    value = "default"
+  metadata {
+    name = "istio-system"
+    labels = {
+      "field.cattle.io/projectId" = "system"
+      "topology.istio.io/network" = "network1"
+    }
+    annotations = {
+      "management.cattle.io/system-namespace" : "true"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata.0.labels,
+      metadata.0.annotations,
+    ]
   }
 }
 
@@ -18,24 +26,52 @@ resource "helm_release" "istio_guppy" {
   repository       = "https://istio-release.storage.googleapis.com/charts"
   chart            = "base"
   namespace        = "istio-system"
-  create_namespace = true
 
   set {
     name = "defaultRevision"
     value = "default"
   }
+
+  depends_on = [ kubernetes_namespace.istio_guppy ]
 }
 
-resource "helm_release" "istiod_tetra" {
+resource "kubernetes_namespace" "istio_tetra" {
+  provider         = kubernetes.tetra
+
+  metadata {
+    name = "istio-system"
+    labels = {
+      "field.cattle.io/projectId" = "system"
+      "topology.istio.io/network" = "network2"
+    }
+    annotations = {
+      "management.cattle.io/system-namespace" : "true"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata.0.labels,
+      metadata.0.annotations,
+    ]
+  }
+}
+resource "helm_release" "istio_tetra" {
   provider         = helm.tetra
-  name             = "istiod"
+  name             = "istio-base"
   repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "istiod"
+  chart            = "base"
   namespace        = "istio-system"
 
-  depends_on = [ helm_release.istio_tetra ]
+  set {
+    name = "defaultRevision"
+    value = "default"
+  }
+
+  depends_on = [ kubernetes_namespace.istio_tetra ]
 }
 
+// Istio control plane guppy
 resource "helm_release" "istiod_guppy" {
   provider         = helm.guppy
   name             = "istiod"
@@ -43,27 +79,32 @@ resource "helm_release" "istiod_guppy" {
   chart            = "istiod"
   namespace        = "istio-system"
 
-  depends_on = [ helm_release.istio_guppy ]
-}
-
-resource "helm_release" "istio_ingress_tetra" {
-  provider         = helm.tetra
-  name             = "istio-ingress"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "gateway"
-  namespace        = "istio-ingress"
-  create_namespace = true
-
   set {
-    name  = "defaults.service.externalIPs[0]"
-    value = tolist(data.kubernetes_service.nginx_ingress_tetra.spec.0.external_ips)[0]
+    name = "defaults.global.meshID"
+    value = "shoal"
   }
 
-  values = [
-    file("${path.module}/helm/istio_ingress/values.yml"),
-  ]
+  set {
+    name = "defaults.global.multiCluster.clusterName"
+    value = "guppy"
+  }
 
-  depends_on = [ helm_release.istiod_tetra ]
+  set {
+    name = "defaults.global.multiCluster.enabled"
+    value = true
+  }
+
+  set {
+    name = "defaults.global.network"
+    value = "guppy"
+  }
+
+  set {
+    name = "defaults.global.externalIstiod"
+    value = true
+  }
+
+  depends_on = [ helm_release.istio_guppy ]
 }
 
 resource "helm_release" "istio_ingress_guppy" {
@@ -86,8 +127,59 @@ resource "helm_release" "istio_ingress_guppy" {
   depends_on = [ helm_release.istiod_guppy ]
 }
 
-resource "helm_release" "kiali" {
+// Istio control plane tetra
+resource "helm_release" "istiod_tetra" {
   provider         = helm.tetra
+  name             = "istiod"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "istiod"
+  namespace        = "istio-system"
+
+  set {
+    name = "defaults.global.meshID"
+    value = "shoal"
+  }
+
+  set {
+    name = "defaults.global.multiCluster.clusterName"
+    value = "tetra"
+  }
+
+  set {
+    name = "defaults.global.multiCluster.enabled"
+    value = true
+  }
+
+  set {
+    name = "defaults.global.network"
+    value = "tetra"
+  }
+
+  depends_on = [ helm_release.istio_tetra ]
+}
+
+resource "helm_release" "istio_ingress_tetra" {
+  provider         = helm.tetra
+  name             = "istio-ingress"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "gateway"
+  namespace        = "istio-ingress"
+  create_namespace = true
+
+  set {
+    name  = "defaults.service.externalIPs[0]"
+    value = tolist(data.kubernetes_service.nginx_ingress_tetra.spec.0.external_ips)[0]
+  }
+
+  values = [
+    file("${path.module}/helm/istio_ingress/values.yml"),
+  ]
+
+  depends_on = [ helm_release.istiod_tetra ]
+}
+
+resource "helm_release" "kiali" {
+  provider         = helm.guppy
   name             = "kiali"
   repository       = "https://kiali.org/helm-charts"
   chart            = "kiali-server"
