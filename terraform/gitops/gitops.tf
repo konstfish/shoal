bfb4da5ada73fe470358faee687e31266aa841a8
David Fischer <david@konst.fish>
2024-08-22 00:50:39 +0200

    feat(gitops): restructure operators

--- terraform/gitops/gitops.tf
+++
@@ -1,8 +0,0 @@
-
-resource "kubernetes_manifest" "fleet_projects_local" {
-  manifest = yamldecode(file("${path.module}/kubernetes/fleet-local.yaml"))
-}
-
-resource "kubernetes_manifest" "fleet_projects_default" {
-  manifest = yamldecode(file("${path.module}/kubernetes/fleet-default.yaml"))
-}
