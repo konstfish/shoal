4c52605c6e9b53972053145bf97edc5e4727df03
David Fischer <david@konst.fish>
2024-08-25 01:31:22 +0200

    fix(hetzner/ccm): unfortunately needs 2 be like this

--- rancher/modules.tf
+++ rancher/modules.tf
@@ -3,15 +3,12 @@ module "cert_manager" {
   source = "../terraform/cert_manager"
 
   cloudflare_api_token = var.cloudflare_api_token
+
+  depends_on = [ helm_release.ccm ]
 }
 
 module "rancher" {
   source = "../terraform/rancher"
 
   depends_on = [module.cert_manager]
-}
-resource "kubernetes_manifest" "fleet_projects" {
-  manifest = yamldecode(file("${path.module}/kubernetes/fleet-local.yaml"))
-
-  depends_on = [module.rancher]
 }
