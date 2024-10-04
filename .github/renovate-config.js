e8b744af65e6c40999f43ef89878563608839c17
David Fischer <david@konst.fish>
2024-10-04 22:55:13 +0200

    feat(renovate): go live

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -4,21 +4,11 @@ module.exports = {
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     onboarding: false,
     platform: 'github',
-    forkProcessing: 'enabled',
-    dryRun: 'full',
     repositories: ['konstfish/shoal'],
     packageRules: [
       {
-        description: 'lockFileMaintenance',
-        matchUpdateTypes: [
-          'pin',
-          'digest',
-          'patch',
-          'minor',
-          'major',
-          'lockFileMaintenance',
-        ],
-        dependencyDashboardApproval: false,
-      },
-    ],
+        matchUpdateTypes: ['minor', 'patch', 'pin', 'digest'],
+        automerge: true
+      }
+    ]
   };
