464fc54496fd5cbab313665d2ced63b0800050b8
David Fischer <david@konst.fish>
2024-10-05 17:18:06 +0200

    chore(repo): move old terraform modules to archive

--- anchovy/main.tf
+++
@@ -1,3 +0,0 @@
-module "openpolicyagent" {
-  source = "../terraform/openpolicyagent"
-}
---
+++ rancher/rancher.tf
@@ -0,0 +1,30 @@
+// core
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "1.14.5"
+
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
+
+resource "helm_release" "rancher" {
+  name             = "rancher"
+  repository       = "https://releases.rancher.com/server-charts/latest"
+  chart            = "rancher"
+  namespace        = "cattle-system"
+  version          = "2.9.2"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/helm/rancher.yaml")
+  ]
+  
+  depends_on = [helm_release.cert_manager]
+}
