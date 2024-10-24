4c1441aae8c848fdfa0a956c73de5d64f42e2758
David Fischer <david@konst.fish>
2024-10-24 23:00:12 +0200

    feat(rancher): upgrade & fix terraform state

--- clusters/barracuda/rancher/providers.tf
+++ clusters/barracuda/rancher/providers.tf
@@ -14,12 +14,12 @@ terraform {
 
 provider "helm" {
   kubernetes {
-    config_path = "${path.module}/../barracuda/.terraform/modules/k3s/artifacts/barracuda.yml"
+    config_path = "${path.module}/../.terraform/modules/k3s/artifacts/barracuda.yml"
     insecure    = "true"
   }
 }
 
 provider "kubernetes" {
-  config_path = "${path.module}/../barracuda/.terraform/modules/k3s/artifacts/barracuda.yml"
+  config_path = "${path.module}/../.terraform/modules/k3s/artifacts/barracuda.yml"
   insecure    = "true"
 }
